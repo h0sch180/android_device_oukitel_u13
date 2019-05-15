10c48b3198423528a6368149202f06ce73781dc2
h0sch180 <hoschi@anukis.de>
2019-05-15 16:59:17 +0200

    minor fix

--- viper.mk
+++ viper.mk
@@ -17,7 +17,7 @@
 LOCAL_PATH := device/OUKITEL/U13
 
 # Inherit some common Lineage OS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/viper/config/common_full_phone.mk)
 # Use this to add NOTE_S to CM's lunch command menu
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
