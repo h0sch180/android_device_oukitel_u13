e9606b48fb7a134711b7eca4993e3ae4215ea8e1
h0sch180 <hoschi@anukis.de>
2019-09-04 23:32:31 +0200

    fix compile error

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage.mk
+    $(LOCAL_DIR)/aosp_U13.mk
--- aosp_U13.mk
+++ aosp_U13.mk
@@ -1,31 +1,34 @@
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright 2014 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+
+# Sample: This is where we'd set a backup provider if we had one
+# $(call inherit-product, device/sample/products/backup_overlay.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+$(call inherit-product, device/OUKITEL/U13/device_U13.mk)
+$(call inherit-product-if-exists, vendor/OUKITEL/U13/U13-vendor.mk)
 
 LOCAL_PATH := device/OUKITEL/U13
 
 # Inherit some common Lineage OS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-# Use this to add NOTE_S to CM's lunch command menu
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/OUKITEL/U13/lineage_U13.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Inherit device configuration
 #$(call inherit-product, $(LOCAL_PATH)/device_U13.mk)
